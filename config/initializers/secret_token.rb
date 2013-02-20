1faffaccdfd5c321e0b015bfead6905eba60993c
Melvin Sembrano <melvinsembrano@gmail.com>
2013-02-20 22:23:17 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stewk::Application.config.secret_token = '06f4177c5e809c8603512f5e9c3d9ee6ddd41487b821c8c510e178a267bb25b96ef7d2049ae19c55873deb0e8ecf9bb31570fb312b7e049e38eab253d97db8f3'

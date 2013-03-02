6d656699430fdb7a9395931e0848dafc167d9016
Melvin Sembrano <melvinsembrano@gmail.com>
2013-03-02 11:16:36 +1100

    added due on

--- Gemfile
+++ Gemfile
@@ -26,6 +26,11 @@ end
 
 gem 'jquery-rails'
 
+group :test do
+  gem 'ruby-prof'
+  gem 'cucumber'
+end
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,22 +1,32 @@
 class Todo < ActiveRecord::Base
   attr_accessor :due_date_on, :due_time_on
-  attr_accessible :account_id, :description, :due_on, :title, :type_id
+  attr_accessible :account_id, :description, :due_on, :title, :type_id, :due_date_on, :due_time_on
   belongs_to :account
 
   before_save :set_due_on
 
   def due_date_on
-    due_on.to_date if due_on
+    if @due_date_on.nil? && due_on
+      @due_date_on = due_on.to_date.to_s
+    end
+    @due_date_on
   end
 
   def due_time_on
-    due_on.to_time if due_on
+    if @due_time_on.nil? && due_on
+      @due_time_on = due_on.to_time.strftime("%T")
+    end
+    @due_time_on
+  end
+
+  def parsed_due_on
+    DateTime.parse("#{ due_date_on } #{ due_time_on }")
   end
 
   private
 
   def set_due_on
-
+    due_on = parsed_due_on
   end
 
 end
